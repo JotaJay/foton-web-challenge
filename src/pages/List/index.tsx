243f71baad8ee36001bb5f16f9ece03c01d236b7
Junior <reinaldo.diasjunior@gmail.com>
2021-02-11 09:58:59 -0300

    Starting flexbox list

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -39,6 +39,42 @@ const List: React.FC = () => {
         </form>
         <button onClick={getBooks}>Search</button>
         <Content>
+          <div>
+            <img
+              src="http://books.google.com/books/content?id=_rtFAQAAIAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              src="http://books.google.com/books/content?id=_rtFAQAAIAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              src="http://books.google.com/books/content?id=_rtFAQAAIAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              src="http://books.google.com/books/content?id=_rtFAQAAIAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              src="http://books.google.com/books/content?id=_rtFAQAAIAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              src="http://books.google.com/books/content?id=_rtFAQAAIAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+              alt=""
+            />
+          </div>
           {books?.map((book) => {
             <div>
               <img src={book.volumeInfo.imageLinks.thumbail} alt="" />
--- src/pages/List/style.ts
+++ src/pages/List/style.ts
@@ -4,4 +4,17 @@ export const Container = styled.div``;
 
 export const Header = styled.div``;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+  max-width: 960px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+
+  div {
+    min-width: 200px;
+  }
+
+  img {
+    display: block;
+  }
+`;
