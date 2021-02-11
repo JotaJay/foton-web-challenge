fb23f491ad7ca7e51137fcfb89ca1c259e66c4ef
Junior <reinaldo.diasjunior@gmail.com>
2021-02-11 11:56:41 -0300

    Adds book page

--- src/pages/Book/index.tsx
+++ src/pages/Book/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useRouteMatch } from "react-router-dom";
 
+import { Header, Content, Card, Description } from "./style";
+
 interface QueryParams {
   book: string;
 }
@@ -9,7 +11,33 @@ const Book: React.FC = () => {
   const { params } = useRouteMatch<QueryParams>();
   return (
     <>
-      <h1>{params.book}</h1>
+      <Header></Header>
+      <Content>
+        <Card>
+          <img
+            src={
+              "http://books.google.com/books/content?id=_rtFAQAAIAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+            }
+          />
+          <div>
+            <strong>Descrição</strong>
+            <p>autor</p>
+          </div>
+          <div>
+            <span>paginas</span>
+            <button>like</button>
+            <button>favorite</button>
+          </div>
+        </Card>
+        <Description>
+          <div>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Praesentium voluptate consequuntur quam iusto corrupti ex magni!
+            Quae libero est ad architecto, quaerat sint obcaecati hic
+            accusantium, repudiandae culpa qui quia!
+          </div>
+        </Description>
+      </Content>
     </>
   );
 };
---
+++ src/pages/Book/style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Header = styled.div``;
+
+export const Content = styled.div``;
+
+export const Card = styled.div``;
+
+export const Description = styled.div`
+  margin: 0 auto;
+  width: 100vw;
+  position: relative;
+  background: #fff;
+`;
