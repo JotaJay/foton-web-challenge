96e1d6b8692577fff1402f6cc102af6b77c80014
Junior <reinaldo.diasjunior@gmail.com>
2021-02-11 10:30:02 -0300

    Flexbox adjustments

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -38,50 +38,50 @@ const List: React.FC = () => {
           <input type="text" name="book" id="book" />
         </form>
         <button onClick={getBooks}>Search</button>
-        <Content>
-          <div>
-            <img
-              src="http://books.google.com/books/content?id=_rtFAQAAIAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              src="http://books.google.com/books/content?id=_rtFAQAAIAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              src="http://books.google.com/books/content?id=_rtFAQAAIAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              src="http://books.google.com/books/content?id=_rtFAQAAIAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              src="http://books.google.com/books/content?id=_rtFAQAAIAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              src="http://books.google.com/books/content?id=_rtFAQAAIAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
-              alt=""
-            />
-          </div>
-          {books?.map((book) => {
-            <div>
-              <img src={book.volumeInfo.imageLinks.thumbail} alt="" />
-            </div>;
-          })}
-        </Content>
       </Header>
+      <Content>
+        <div>
+          <img
+            src="http://books.google.com/books/content?id=_rtFAQAAIAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            src="http://books.google.com/books/content?id=_rtFAQAAIAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            src="http://books.google.com/books/content?id=_rtFAQAAIAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            src="http://books.google.com/books/content?id=_rtFAQAAIAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            src="http://books.google.com/books/content?id=_rtFAQAAIAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            src="http://books.google.com/books/content?id=_rtFAQAAIAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+            alt=""
+          />
+        </div>
+        {books?.map((book) => {
+          <div>
+            <img src={book.volumeInfo.imageLinks.thumbail} alt="" />
+          </div>;
+        })}
+      </Content>
     </Container>
   );
 };
--- src/pages/List/style.ts
+++ src/pages/List/style.ts
@@ -2,13 +2,16 @@ import styled from "styled-components";
 
 export const Container = styled.div``;
 
-export const Header = styled.div``;
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
 
 export const Content = styled.div`
   max-width: 960px;
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-evenly;
+  justify-content: flex-start;
 
   div {
     min-width: 200px;
