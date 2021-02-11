f3be97efdb162c2b1b78fc40145d5aef2509255a
Junior <reinaldo.diasjunior@gmail.com>
2021-02-11 14:22:04 -0300

    Changes root css

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,14 @@
-import React from "react"
+import React from "react";
+import { Background, Container } from "./style";
 
-const Home: React.FC = ()=>{
-    return <h1>HOME</h1>
-}
+const Home: React.FC = () => {
+  return (
+    <Background>
+      <Container>
+        <h1>HOME</h1>
+      </Container>
+    </Background>
+  );
+};
 
-export default Home
+export default Home;
---
+++ src/pages/Home/style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Background = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background: #ffe43b;
+`;
+
+export const Container = styled.div`
+  max-width: 960px;
+  padding: 40px 20px;
+  margin: 0 auto;
+`;
